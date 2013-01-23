c7c0ab02190557f83746af2891e5cacd3640be78
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-01-24 00:41:27 +0400

    db models

--- boot.js
+++ boot.js
@@ -1,6 +1,11 @@
 require('./lib/i18n-maker').make(function (err, langs) {
-    if (!err)
-        require('./lib/app').start(langs);
-    else
+    if (!err){
+        require('./lib/db').dbStart(function (err) {
+            if(!err) {
+                require('./lib/app').start(langs);
+            }
+        });
+    } else {
         console.log('error compiling languages');
+    }
 });
