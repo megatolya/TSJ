c85f8c1a78fe2d2dd3ea71d3598711ef31217846
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-01-25 00:27:17 +0400

    basic GET api

--- boot.js
+++ boot.js
@@ -1,6 +1,6 @@
 require('./lib/i18n-maker').make(function (err, langs) {
     if (!err){
-        require('./lib/db').dbStart(function (err) {
+        require('./lib/db').start(function (err) {
             if(!err) {
                 require('./lib/app').start(langs);
             }
