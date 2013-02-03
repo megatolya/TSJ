9da7693254843cd3f93e397bd2387e694de9df9f
Anatoly Ostrovskiy <megatolya@yandex-team.ru>
2013-02-04 02:25:01 +0400

    stable

--- static/js/ios.js
+++ static/js/ios.js
@@ -1,20 +1,38 @@
 $(function () {
-    $('a').click(function () {
+    $('a').mousedown(function () {
         var href  = $(this).attr('href');
         if (typeof href !== 'undefined' && href !== false) {
             window.location.href=href;
         }
     });
-    $('.show-im-form').click(function () {
+    $('.show-im-form').mousedown(function () {
         $(this).hide();
         $('.im-form').show();
     });
-    $('.segmented-controller a').click(function() {
+    $('.segmented-controller a').mousedown(function() {
         $('.segmented-controller li').removeClass('active');
         $(this).parent().addClass('active');
         $('.call').val($(this).data('call'));
     });
-    $('.pay').click(function(){
+    var path = URL.parse(location.href).path;
+    if ( path == '/im' ) {
+        var interval = setInterval(function() {
+            $.ajax({
+                url: '/im-ajax',
+                type: 'GET',
+                success : function(data) {
+                    console.log(data);
+                    var html = Templating.tpl('ios-ajax-im.jade', data);
+                    $('.ims').html(html);
+                },
+                error : function(data) {
+                    alert('error!');
+                    clearInterval(interval);
+                }
+            });
+        },900);
+    }
+    $('.pay').mousedown(function(){
         var pay = prompt('Сколько положить на счет?');
         if (pay) {
             $.ajax({
@@ -29,6 +47,5 @@ $(function () {
             });
 
         };
-
     });
 });
