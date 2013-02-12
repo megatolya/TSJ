3b0eaf9e1bf870da8054de8ea97574b92269428c
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-02-12 23:32:27 +0400

    db management

--- static/js/admin.js
+++ static/js/admin.js
@@ -69,4 +69,46 @@ $(function() {
             }
         });
     });
+    $('.get-db').click(function() {
+        var link = $(this);
+        var table = link.data('get');
+
+        $.ajax({
+            url: '/db/' + table,
+            type: 'GET',
+            success : function(data) {
+                var html = Templating.tpl('db-table.jade', data);
+                var content = $(link.parents('.table').find('.table-content'))
+                content.html(html);
+                $(content.find('input')).blur(function(){
+                    var input = $(this);
+                    var table = input.parents('table').data('table');
+                    var key = input.data('key');
+                    var value = input.val();
+                    var oldValue = input.data('old-val');
+                    if (oldValue != value) {
+                        var id = input.parents('tr').addClass('success').data('id');
+                        var req = {
+                            id: id,
+                            table: table,
+                            key: key,
+                            value: value
+                        };
+                        $.ajax({
+                            url: '/db/save',
+                            type: 'POST',
+                            data: req,
+                            success : function(data) {
+                                console.log(data);
+                                input.parents('tr').removeClass('success');
+                            }
+                        });
+                    }
+                });
+            }, error : function(data) {
+                console.log('error');
+                console.log(data);
+            }
+        });
+    });
 });
