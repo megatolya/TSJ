b4bbed2a44995985762657ff8a7fe748772fc0fd
Anatoliy Ostrovskiy <megatolya@yandex-team.ru>
2013-04-13 23:16:37 +0400

    logger + some fixs

--- boot.js
+++ boot.js
@@ -1,13 +1,15 @@
+var logger = require('./lib/logger');
+
 require('./lib/i18n-maker').make(function (err, langs) {
     if (!err){
         require('./lib/db').start(function (err) {
             if(!err) {
                 require('./lib/app').start(langs);
             } else {
-                console.log(err);
+                logger.error(err);
             }
         });
     } else {
-        console.log('error compiling languages');
+        logger.error('Error compiling languages');
     }
 });
